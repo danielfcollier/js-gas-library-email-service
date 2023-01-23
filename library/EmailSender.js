5a47f2c46ece87ed0691e91665f97660722ed1ed
danielfcollier <danielfcollier@gmail.com>
2023-01-23 00:51:20 -0300

    Email sender library.

---
+++ library/EmailSender.js
@@ -0,0 +1,35 @@
+function EmailSender(messageParams, emailParams, mock = false) {
+  const htmlTemplate = getHtmlTemplate();
+  const htmlMessage = getHtmlMessage(messageParams, htmlTemplate);
+  const emailBody = {
+    to: emailParams.destination,
+    bcc: emailParams.origin,
+    subject: emailParams.subjectLine,
+    htmlBody: htmlMessage,
+    name: emailParams.source,
+    replyTo: emailParams.origin,
+  };
+
+  if (mock) {
+    console.log("Done nothing...");
+    console.log({ emailBody });
+    return true;
+  }
+
+  try {
+    if (emailParams?.card) {
+      MailApp.sendEmail({
+        ...emailBody,
+        inlineImages: {
+          myLogo: emailParams.card,
+        },
+      });
+    } else {
+      MailApp.sendEmail(emailBody);
+      return true;
+    }
+  } catch {
+    console.log(`Error while trying to send email.`);
+    return false;
+  }
+}
